3ae83f126634e992e626de8bdbdf69ad65f55789
franciscofigueroamHackMonkeys <81710477+franciscofigueroamHackMonkeys@users.noreply.github.com>
2022-05-15 19:05:02 -0400

    15-05-2022 07:04
    - Cambios footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,13 @@ import { Box, Typography } from "@mui/material";
 //EL SGTE. COMPONENTE RETORNA UNA BARRA CENTRADA EN EL FOOTER DE NUESTRO PROYECTO
 const Footer = () => {
   return (
-    <Box component="div" textAlign="center" pb={3} pt={3} mb={0} sx={{ backgroundColor: "#424242" }}>
+    <Box
+      component="div"
+      textAlign="center"
+      pt={3}
+      pb={3}
+      sx={{ backgroundColor: "#424242" }}
+    >
       <Typography variant="h6" color="#FFFFFF">
         Web development carried out by Francisco Figueroa.
       </Typography>
